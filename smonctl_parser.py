ad759d2b7102afd4e5bf4466dc9c7d0794729b28
Aviolat Romain <romain.aviolat@nagra.com>
2017-07-19 16:16:52 +0200

    Few typos

--- smonctl_parser.py
+++ smonctl_parser.py
@@ -9,18 +9,18 @@ proc = subprocess.Popen(['/usr/sbin/smonctl', '-j'], stdout=subprocess.PIPE)
 smonctl_output = json.loads(proc.stdout.read())
 
 for i in smonctl_output:
-   name = i['name'].lower()
-   descr = i['description']
-   state = i['state']
-   type = i['type']
+    name = i['name'].lower()
+    descr = i['description']
+    state = i['state']
+    metric_type = i['type']
 
-   if i['state'] == "OK":
-      value = 1
-   elif i['state'] == "ABSENT":
-      value == -1
-   elif i['state'] == "BAD":
-      value == 0
+    if i['state'] == "OK":
+       value = 1
+    elif i['state'] == "ABSENT":
+       value == -1
+    elif i['state'] == "BAD":
+       value == 0
 
-   print("# HELP smon_%s %s" % (type, descr))
-   print("# TYPE smon_%s gauge" % type)
-   print("smon_%s{name='%s'} %s" % (type,name,value))
+    print("# HELP smon_%s %s" % (metric_type, descr))
+    print("# TYPE smon_%s gauge" % metric_type)
+    print('smon_%s{name="%s"} %s' % (metric_type,name,value))
